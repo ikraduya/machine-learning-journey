0f2b7b9d94ea9e41f10e5a82255375f9af8ae32e
ikraduya <ikraduya@gmail.com>
2019-01-28 21:37:06 +0700

    add housing price decision tree

---
+++ decision-tree/housing-price-decision-tree/main.py
@@ -0,0 +1,45 @@
+# Predict Housing Price using Decision Tree Regressor Sklearn
+# author: ikraduya
+
+# imports
+import pandas as pd
+
+# path to the data
+file_path = 'data/train.csv'
+
+train_data = pd.read_csv(file_path)
+
+y = train_data.SalePrice  # extract the target predicted value
+
+# choosing "Features"
+feature_list = ['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street',
+       'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig',
+       'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
+       'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd',
+       'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType',
+       'MasVnrArea', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual',
+       'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1',
+       'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'Heating',
+       'HeatingQC', 'CentralAir', 'Electrical', '1stFlrSF', '2ndFlrSF',
+       'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath',
+       'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'KitchenQual',
+       'TotRmsAbvGrd', 'Functional', 'Fireplaces', 'FireplaceQu', 'GarageType',
+       'GarageYrBlt', 'GarageFinish', 'GarageCars', 'GarageArea', 'GarageQual',
+       'GarageCond', 'PavedDrive', 'WoodDeckSF', 'OpenPorchSF',
+       'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC',
+       'Fence', 'MiscFeature', 'MiscVal', 'MoSold', 'YrSold', 'SaleType',
+       'SaleCondition']
+
+selected_features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = train_data[selected_features]
+
+# create and fit model
+from sklearn.tree import DecisionTreeRegressor
+
+housing_price_model = DecisionTreeRegressor(random_state=1)
+# fit model
+housing_price_model.fit(X, y)
+predictions = housing_price_model.predict(X)
+
+print(y.head())
+print(predictions[:5])
